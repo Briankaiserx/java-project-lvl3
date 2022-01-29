5a62867746bbd92d7c3f04958c00214e8acb36c1
Briankaiserx <Briankaiser@mail.ru>
2022-01-29 16:24:13 +0400

    Hexlet

--- src/main/java/hexlet/code/NumberSchema.java
+++ src/main/java/hexlet/code/NumberSchema.java
@@ -1,16 +1,16 @@
 package hexlet.code;
 
-public class NumberSchema extends BaseSchema{
+public class NumberSchema extends BaseSchema {
 
-    public NumberSchema required(){
+    public final NumberSchema required() {
         addRequirement(x -> x instanceof  Integer);
         return this;
     }
-    public NumberSchema range(int minNum, int maxNum){
+    public final NumberSchema range(int minNum, int maxNum) {
         addRequirement(x -> x instanceof Integer && (int) x >= minNum && (int) x <= maxNum);
         return this;
     }
-    public NumberSchema positive() {
+    public final NumberSchema positive() {
         addRequirement(x -> x instanceof Integer && (Integer) x > 0);
         return this;
 }
--- src/test/java/hexlet/code/TestValidator.java
+++ src/test/java/hexlet/code/TestValidator.java
@@ -24,29 +24,31 @@ public class TestValidator {
         assertTrue(schema.contains("what").isValid("what does the fox say"));
         assertFalse(schema.contains("whatthe").isValid("what does the fox say"));
     }
+    private final int nimNumber = 3;
+    private final int maxNumber = 8;
     @Test
-    public void testValidatorNumber(){
+    public void testValidatorNumber() {
         Validator v = new Validator();
         NumberSchema schema = v.numberSchema();
         assertTrue(schema.isValid(null));
 
         schema.required();
 
-        assertTrue(schema.isValid(8));
+        assertTrue(schema.isValid(maxNumber));
         assertFalse(schema.isValid("5"));
         assertFalse(schema.isValid(null));
 
 
-        assertTrue(schema.positive().isValid(8)); // true
-        assertFalse(schema.isValid(-8)); // false
-        assertFalse(schema.positive().isValid(null)); // true
+        assertTrue(schema.positive().isValid(maxNumber));
+        assertFalse(schema.isValid(-maxNumber)); // false
+        assertFalse(schema.positive().isValid(null));
 
         schema.required();
-        schema.range(3, 8);
-        assertTrue(schema.isValid(3)); // true
-        assertTrue(schema.isValid(8)); // true
-        assertFalse(schema.isValid(3 - 1)); // false
-        assertFalse(schema.isValid(8 + 1)); // false
+        schema.range(nimNumber, maxNumber);
+        assertTrue(schema.isValid(nimNumber));
+        assertTrue(schema.isValid(maxNumber));
+        assertFalse(schema.isValid(nimNumber - 1));
+        assertFalse(schema.isValid(maxNumber + 1));
     }
 
 }
